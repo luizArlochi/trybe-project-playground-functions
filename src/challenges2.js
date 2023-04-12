4b19365ea64e8c8d172bee9e946e2133944b2188
Luiz Arlochi <luiz.arlochi@gmail.com>
2023-04-12 17:00:35 -0300

    requisito 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,4 +1,24 @@
 // Desafio 11 - Crie a função generatePhoneNumber
+function generatePhoneNumber(numbers) {
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < numbers.length; index+= 1) {
+    if (numbers[index] < 0 || numbers[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    let count = 0;
+    for (let num = 0; num < numbers.length; num+= 1) {
+      if (numbers[index] === numbers[num]) {
+        count+= 1;
+      }
+    }
+    if (count >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return `(${numbers[0]}${numbers[1]}) ${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`;
+}
 
 // Desafio 12 -  Crie a função triangleCheck
 
